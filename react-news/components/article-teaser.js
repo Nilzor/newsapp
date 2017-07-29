44ddb41f6d929236ec3793fa0fc537f9844a61f8
Frode Nilsen <frode.nilsen@schibsted.com>
2017-07-29 13:01:59 +0200

    Make ArticleTeaser show image and description in a flex box. WIP

--- react-news/components/article-teaser.js
+++ react-news/components/article-teaser.js
@@ -1,20 +1,50 @@
-
 import React, { Component } from 'react';
 import {
     StyleSheet,
     Text,
     View
     } from 'react-native';
+
 export default class ArticleTeaser extends Component {
     render() {
         const article = this.props.article;
+        var image = getFittingImage(article);
+        // Improvement: Instead of wrapping flex's: https://codepen.io/vkjgr/pen/OPRPRR
         return (
-            <Text style={styles.text}>{article.promotionContent.title.value}{'\n'}{'\n'}</Text>
+            <View
+                style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+                <Image
+                    style={{width: 300, margin: 4}}
+                    source={{uri: image.url}}
+                    />
+                <View style={{flex: 1, flexDirection: 'column'}}>
+                    <Text style={styles.title}>{article.promotionContent.title.value}</Text>
+                    <Text style={styles.description}>{article.promotionContent.description.value}</Text>
+                </View>
+            </View>
+
         )
+    }
+
+    getFittingImage(article) {
+        const minWidth = 300;
+        for (var asset in article.promotionContent.imageAsset) {
+            if (asset.width > minWidth) {
+                return asset;
+            }
+        }
+        return null;
     }
 }
 
 const styles = StyleSheet.create({
-    text: {
-    }
+    title: {
+        margin: 4,
+        fontWeight: "bold",
+        fontSize: 16
+    },
+    description: {
+        margin: 4,
+        fontSize: 14
+    },
 });
