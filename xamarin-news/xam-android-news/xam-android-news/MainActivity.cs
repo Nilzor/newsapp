3a1aeae81dbba44d3142f2018d19331dbec87d8d
Frode Nilsen <nilzor@gmail.com>
2017-08-27 10:01:00 +0200

    Blank Xamarin Android project

---
+++ xamarin-news/xam-android-news/xam-android-news/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace xam_android_news
+{
+    [Activity(Label = "xam_android_news", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
