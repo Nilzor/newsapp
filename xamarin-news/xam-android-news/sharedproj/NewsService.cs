53719dd1cdd97d06becccfd3dae4e77106f56d64
Frode Nilsen <frode.nilsen@schibsted.com>
2017-08-28 21:11:28 +0200

    Add RestSharp library dependency

---
+++ xamarin-news/xam-android-news/sharedproj/NewsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sharedproj
+{
+    class NewsService
+    {
+        public const url = "http://sch.nilsenlabs.com/data/latest";
+    }
+}
