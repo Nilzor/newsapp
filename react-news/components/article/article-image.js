894ce619b03352d5023599afbd8bf3dd1266a9a2
Frode Nilsen <nilzor@gmail.com>
2017-09-28 21:26:50 +0200

    React: Fix caption for images

--- react-news/components/article/article-image.js
+++ react-news/components/article/article-image.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {
     Image,
+    Text,
+    View,
 } from 'react-native';
 import _ImageHelper from '../../common/imagehelper';
 const ImageHelper = new _ImageHelper();
@@ -10,24 +12,41 @@ export default class ArticleImage extends Component {
         const data = this.props.data;
         const image = ImageHelper.getFittingImage(data.imageAsset, 999999);
         return (
-            <Image
-                style={{
-                   width: 400,
-                   height: 200,
-                   marginTop: 4,
-                   marginBottom: 4,
-                   borderWidth: 1,
-                   borderColor: '#777',
-               }}
-                source={{uri: image.url}}
-            >
-            </Image>
+            <View>
+                <Image
+                    style={{
+                       width: 400,
+                       height: 200,
+                       marginTop: 4,
+                       marginBottom: 4,
+                       borderWidth: 1,
+                       resizeMode: 'contain',
+                       borderColor: '#777',
+                   }}
+                    source={{uri: image.url}}
+                >
+                </Image>
+                <Text style={[commonStyles.text, commonStyles.stdPadding, styles.caption]}>
+                    {data.caption.value}
+                </Text>
+                <Text style={[commonStyles.text, commonStyles.stdPadding, styles.byLine]}>
+                    {data.byline.title}
+                </Text>
+            </View>
         )
     }
 }
 
 const styles = {
-    standard: {
-        margin: 4
+    caption: {
+        fontSize: 14,
+        fontStyle: 'italic',
+        paddingBottom: 2
+    },
+    byLine: {
+        fontSize: 14,
+        fontStyle: 'italic',
+        color: "#555",
+        paddingTop: 0
     }
 };
