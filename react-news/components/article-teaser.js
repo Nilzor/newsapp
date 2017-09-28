896bb30258b726781d5d078d38afd2f7f1f16f53
Frode Nilsen <nilzor@gmail.com>
2017-09-28 22:01:19 +0200

    React: List now template for other apps.

--- react-news/components/article-teaser.js
+++ react-news/components/article-teaser.js
@@ -6,6 +6,7 @@ import {
     Image,
     TouchableHighlight,
     } from 'react-native';
+import commonStyles from '../styles/common';
 import _ImageHelper from '../common/imagehelper';
 const ImageHelper = new _ImageHelper();
 
@@ -17,29 +18,19 @@ export default class ArticleTeaser extends Component {
         return (
             <TouchableHighlight onPress={() => this.onClick()} underlayColor="white">
                 <View
-                    style={{
-                        flex: 1,
-                        flexDirection: 'row',
-                        //borderColor: '#ccc',
-                        //borderWidth: 1,
-                        margin: 4,
-                        shadowColor: '#777',
-                        // alignItems: 'flex-start'
-                    }}
+                    style={[commonStyles.main, styles.containerStyle]}
                  >
                     <Image
                        style={{
                            width: 100,
                            height: undefined,
                            margin: 4,
-                           borderWidth: 1,
-                           borderColor: '#777',
                        }}
                        source={{uri: image.url}}
-                       resizeMode='center'
+                       resizeMode='cover'
                     />
                     <View style={{flex: 1, flexDirection: 'column', justifyContent: 'flex-start'}}>
-                        <Text style={styles.title}>{article.promotionContent.title.value}</Text>
+                        <Text style={[commonStyles.text, styles.title]}>{article.promotionContent.title.value}</Text>
                         <Text style={styles.description}>{article.promotionContent.description.value}</Text>
                     </View>
                 </View>
@@ -58,12 +49,24 @@ const styles = StyleSheet.create({
     title: {
         margin: 4,
         fontWeight: "bold",
-        fontSize: 16
+        fontSize: 18
     },
     description: {
+        color: '#555',
         marginLeft: 4,
         marginRight: 4,
         marginBottom: 4,
         fontSize: 14
     },
+    containerStyle: {
+        flexDirection: 'row',
+        //borderColor: '#ccc',
+        //borderWidth: 1,
+        margin: 4,
+        // alignItems: 'flex-start'
+    },
+    separator: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 6
+    }
 });
