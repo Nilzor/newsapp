d8159b8efc0b83a49ba482a4b4b36ad9f89d2a4a
Frode Nilsen <nilzor@gmail.com>
2017-09-30 11:48:05 +0200

    Setting mainLauncher=true again

--- xamarin-news/xam-android-news/xam-android-news/Activities/MainActivity.cs
+++ xamarin-news/xam-android-news/xam-android-news/Activities/MainActivity.cs
@@ -15,7 +15,7 @@ using xam_android_news.Messages;
 
 namespace xam_android_news
 {
-    [Activity(Label = "@string/ApplicationName", MainLauncher = false, Icon = "@drawable/icon")]
+    [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         private const string TAG = "MainActivity";
